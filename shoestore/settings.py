294b185010f8e909c63773e1f1cae8126119109c
Vishnu Krishnakumar <83391274+vishnukkrishna@users.noreply.github.com>
2024-01-03 11:51:24 +0530

    google authentication on is sucessfully updated

--- shoestore/settings.py
+++ shoestore/settings.py
@@ -130,7 +130,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "staffsense222@gmail.com"
-EMAIL_HOST_PASSWORD = "sycrrugwvxxpqjda"
+EMAIL_HOST_PASSWORD = "nbadwahwrhziisjv"
 # kwjqmkwlcpgrkkgy
 EMAIL_USE_TLS = True
 
