57f441db500d6edc909056ae6d58c77c524a4f65
vishnukkrishna <vishnukkrishna1999@gmail.com>
2024-05-31 21:22:17 +0530

    updated static files

--- shoestore/urls.py
+++ shoestore/urls.py
@@ -44,8 +44,7 @@ urlpatterns = [
     path("ad/coupons/", include("coupon.urls")),
     # Admin Order Management
     path("ad/order/", include("orders.urls")),
-]
-+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 
